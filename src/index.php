17562f70c9d93a7231295dc57b8e9d6a6d417362
Elias-Larsson <Elias-Larsson@chasacademy.se>
2024-12-05 14:00:01 +0100

    css/html commit

--- src/index.php
+++ src/index.php
@@ -1,5 +1,6 @@
 <?php
-function sanitize_input($input) {
+function sanitize_input($input)
+{
     $sanitized_input = htmlspecialchars($input);
     $sanitized_input = trim($sanitized_input);
     $sanitized_input = stripslashes($sanitized_input);
@@ -7,7 +8,8 @@ function sanitize_input($input) {
     return $sanitized_input;
 }
 
-function validate_name($input) {
+function validate_name($input)
+{
     if (strlen($input) > 100) {
         return "Invalid format: Name too long";
     }
@@ -15,40 +17,49 @@ function validate_name($input) {
     return $input;
 }
 
-function validate_email($input) {
+function validate_email($input) {}
 
-}
+function validate_subject($input) {}
 
-function validate_subject($input) {
-
-}
-
-function validate_message($input) {
-
-}
+function validate_message($input) {}
 
 if (isset($_POST["name"]) && isset($_POST["email"]) && isset($_POST["subject"]) && isset($_POST["message"])) {
     echo sanitize_input($_POST["name"]);
 }
 ?>
 
-<html>
-    <body>
-        <form action="index.php" method="post">
-            <label>Name:</label>
-            <input type="text" name="name">
+<!DOCTYPE html>
+<html lang="en">
 
-            <label>Email:</label>
-            <input type="email" name="email">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
 
-            <label>Subject:</label>
-            <input type="text" name="subject">
+    <title>Document</title>
+</head>
 
-            <label>Message:</label>
-            <textarea cols="20" rows="5" name="message"></textarea>
+<body>
+    <main>
+        <aside>
+            <h1>Menu</h1>
+            <ul>
+                <li class="list">Lista1</li>
+                <li class="list">Lista1</li>
+                <li class="list">Lista1</li>
+            </ul>
+        </aside>
 
-            <button type=submit>Submit the form</button>
+        <section>
+            <div class="taskItem">
+                <p>task</p>
+            </div>
+            <div class="taskItem">
+                <p>task</p>
+            </div>
+        </section>
 
-        </form>
-    </body>
+    </main>
+</body>
+
 </html>
